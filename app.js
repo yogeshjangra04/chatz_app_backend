6d017aa563d4c49300c3e91c35476d2f6a569f9a
yogeshjangra04 <>
2023-06-25 14:09:14 +0530

    cors socket

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ const { notFound, errorHandler } = require('./middleware/errors')
 
 dotenv.config()
 app.use(express.json())
-// const cors = require('cors');
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://nexus-chat-zeta.vercel.app");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});
-// const corsOptions = {
-//     origin: 'https://nexus-chat-zeta.vercel.app/',
-//     credentials: true,
-//     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-//     allowedHeaders: ['Content-Type'],       //access-control-allow-credentials:true
-//     optionSuccessStatus: 200
-// }
-// app.use(cors(corsOptions));
+const cors = require('cors');
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "https://nexus-chat-zeta.vercel.app");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//     next();
+// });
+const corsOptions = {
+    origin: 'https://nexus-chat-zeta.vercel.app',
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type'],       //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
